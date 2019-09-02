f9839fdffe109081fdd6116f4982b5383b73d479
Russell McWhae <russell.mcwhae@gmail.com>
2019-09-02 13:28:55 -0600

    Adds interviewerList and InterviewerListItem components

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -8,8 +8,8 @@ export default function DayList(props) {
   const days = props.days.map(day => {
     return <DayListItem 
     key={day.id}
-    name={day.name} 
-    spots={day.spots} 
+    name={day.name}
+    spots={day.spots}
     selected={day.name === props.day}
     setDay={props.setDay}  />
   })
---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import "components/InterviewerListItem.scss";
+const classNames = require('classnames');
+
+export default function InterviewerListItem(props) {
+  let interviewerClass = classNames({
+    "interviewers__item": true,
+    "interviewers__item--selected": props.selected,
+ });
+
+  return (
+    <li className={interviewerClass} onClick={() => props.setInterviewer()}>
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && props.name}
+    </li>
+  );
+}
