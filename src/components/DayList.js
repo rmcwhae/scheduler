4e8f94a237c70de03c811be7bc69bb745e572399
Russell McWhae <russell.mcwhae@gmail.com>
2019-09-02 09:31:56 -0600

    Adds unique id key to each DayListItem component

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -7,6 +7,7 @@ import DayListItem from "components/DayListItem";
 export default function DayList(props) {
   const days = props.days.map(day => {
     return <DayListItem 
+    key={day.id}
     name={day.name} 
     spots={day.spots} 
     selected={day.name === props.day}
