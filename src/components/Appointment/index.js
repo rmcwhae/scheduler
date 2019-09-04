df7a336547acac424da7365342e71cf2b0ff7800
Russell McWhae <russell.mcwhae@gmail.com>
2019-09-04 12:27:49 -0600

    Adds ability to add new interview and cancel it

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,13 +4,41 @@ import "./styles.scss";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Form from "components/Appointment/Form";
+import useVisualMode from "hooks/useVisualMode";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+
+
 
 export default function Appointment(props) {
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+  const BACK = "BACK";
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
 
   return (
     <article className="appointment">
     <Header time={props.time}></Header>
-    {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
+    {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+    {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+    )}
+    {mode === CREATE && (
+        <Form
+        interviewers={[]}
+        onSave={""}
+        onCancel={() => back()}
+        />
+    )}
     </article>
   );
 }
