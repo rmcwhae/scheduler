e8ad463b0a6bf5909ecab9a89c02797199aaa67f
Russell McWhae <russell.mcwhae@gmail.com>
2019-09-13 09:46:36 -0600

    Updates transition and back functions to not mutate state

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,24 +3,22 @@ import { useState } from 'react';
 export default function useVisualMode(initial) {
 	const [ mode, setMode ] = useState(initial);
 	const [ history, setHistory ] = useState([ initial ]);
+	const historyCopy = [ ...history ];
 
 	function transition(mode, replace = false) {
 		if (replace) {
-			history.pop();
-			setHistory(history);
-			setMode(mode);
-			setHistory((prev) => [ ...prev, mode ]);
-		} else {
-			setMode(mode);
-			setHistory((prev) => [ ...prev, mode ]);
+			historyCopy.pop();
+			setHistory(historyCopy);
 		}
+		setMode(mode);
+		setHistory((prev) => [ ...prev, mode ]);
 	}
 
 	function back() {
 		if (history.length > 1) {
-			history.pop();
-			setHistory(history);
-			setMode(history.slice(-1)[0]);
+			historyCopy.pop();
+			setHistory(historyCopy);
+			setMode(historyCopy.slice(-1)[0]);
 		} else {
 			setMode(history[0]);
 		}
